71dbdb62290d605cdc1e9430e02eb8da87c19188
Martin Chammah <martin2844@gmail.com>
2020-03-13 18:34:16 -0300

    PORT FIX?

--- app.js
+++ app.js
@@ -204,7 +204,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
     console.log("Server started @ port" + PORT);
 });
