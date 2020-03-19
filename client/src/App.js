500a46215cb16a3f135690f9e2c05585782e993c
unknown <martin2844@gmail.com>
2020-03-19 12:41:28 -0300

    new ui testing & routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,151 +1,30 @@
-import React, { useEffect, useReducer } from 'react';
-import axios from 'axios';
-import Listing from './components/Listing';
+import React from 'react';
 import './App.scss';
-import Spinner from './components/Spinner';
+import {Router, Link} from '@reach/router';
+import Layout from './layout/Layout';
+
+
+
+//import pages
+import About from './pages/About';
+import Profile from './pages/Profile';
+import ThinkpadSearch from './pages/ThinkpadSearch';
+
 
 const App = () => {
 
-  const initialState = {
-    term: "thinkpad",
-    page: 0,
-    loading: true,
-    results: {}
-  }
-
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case 'SEARCH':
-        return {
-          ...state,
-          term: action.term,
-          loading: true,
-        }
-      case 'LOADING': 
-      return {
-        state,
-        loading: true
-      }
-      case 'FINISH':
-        return {
-          ...state,
-          loading: false,
-          results: action.payload
-        }
-      case 'CHANGE_PAGE':
-        return {
-          ...state,
-          page: action.page,
-          loading: true,
-        }
-      default:
-        throw new Error();
-    }
-  }
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-  // const[results, setResults] = useState({});
-
-
-
-    // Initial Search
-    useEffect(() => {
-      console.log("useEffect");
-      axios.get(`/api/search/thinkpad/0`).then((response) => {
-        dispatch({type: 'FINISH', payload: response.data})
-      });  
-    },[])
- 
-    //subsequent searches
-    const updateResults = (term, page) => {
-      axios.get(`/api/search/${term}/${page}`).then((response) => {
-        dispatch({type: 'FINISH', payload: response.data})
-      });
-
-    }
-
-  const displayArticles = () => {
-    if(state.results.articles) {
-      return state.results.articles.map((article) => {
-        let {title, state, city, price, image, link} = article
-        return <Listing key={link} title={title} state={state} city={city} price={price} image={image} link={link} />
-      })
-    }
-  }
-
-  const changePage = (page) => {
-    console.log(page);
-    console.log(state.page);
-    
-    dispatch({type: 'CHANGE_PAGE', page: page})
-    updateResults(state.term, page * 50);
   
-  }
-
-
-  const pagination = () => {
-    let pages = [];
-    for(let i = 0; i < state.results.totalPages; i++) { 
-     
-      pages.push(i);
-
-
-    }
-    console.log(pages);
-    if(pages.length < 2) {
-      return null
-    } else {
-    return pages.map((page) => {
-         // aca hacer si page === to state.page, clase agregada a class name
-      return (
-        
-
-        <span key={page} onClick={() => changePage(page)} className={"pagination-item " + (page === state.page ? "active" : "inactive")}>
-          {page + 1}
-        </span>
-      )
-    })
-  }
-
-  }
-
-  const onClick = (e) =>{
-   
-    let term = e.target.innerText;
-    if(term === "All Thinkpads") {
-      dispatch({type: 'SEARCH', term: 'thinkpad'});
-      updateResults('thinkpad', state.page);
-    } else {
-      dispatch({type: 'SEARCH', term: term});
-      updateResults(term, state.page);
-    }
-
-    
-    
-  }
-
-  
-
   return (
-    <div className="main-container">
-     <h1>Used Thinkpad Meli Search</h1>
-     <h3>My favourite thinkpads</h3>
-     <div className="button-container">
-       <span onClick={e => onClick(e)}>Thinkpad Carbon</span>
-       <span onClick={e => onClick(e)}>Thinkpad T430</span>
-       <span onClick={e => onClick(e)}>Thinkpad T450</span>
-       <span onClick={e => onClick(e)}>Thinkpad W540</span>
-       <span onClick={e => onClick(e)}>All Thinkpads</span>
-     </div>
-     
-      <div style={state.loading ? {minHeight: "500px", alignItems: "center"} : null} className="card-map"> 
-    
-      {state.loading ? <Spinner/> : displayArticles()}
-      
-      </div>
-      <div className="pagination-parent"> {pagination()}</div>
-    </div>
+    <Layout>
+      <Link to="/about">About</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/thinkpad">ThinkpadSearch</Link>
+          <Router>
+              <About path="/about" />
+              <Profile path="/profile" />
+              <ThinkpadSearch path="/thinkpad" />
+        </Router>
+    </Layout>
   )
 }
 
---
+++ client/src/layout/Navbar.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = anchor => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const handleChange = event => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  
+
+  return (
+    <div className={classes.root}>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon onClick={toggleDrawer("left", true)} />
+            <Drawer anchor={"left"} open={state["left"]} onClose={toggleDrawer("left", false)}>
+            {list("left")}
+          </Drawer>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Photos
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
